c024fe0a031c5a23a874fd8e667782ffbb5d6ab5
salmalah <122815714+salmalah@users.noreply.github.com>
2024-01-11 21:33:21 +0100

    Update 5-sum_list.py

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,9 +2,10 @@
 """
 module to Write a type-annotated function sum_list which takes a list of floats
 """
+from typing import List
 
 
-def sum_list(input_list: float) -> float:
+def sum_list(input_list: List[float]) -> float:
     """ take list of float
     return sum"""
     return sum(input_list)

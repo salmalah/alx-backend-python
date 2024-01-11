b3c1620d9c28f18983b090ca3d5b8396770595e7
salmalah <s.salmalahmidi@gmail.com>
2024-01-11 11:55:42 -0800

    1. Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+This is a module the create a function concat type string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Add two string"""
+    return str1 + str2

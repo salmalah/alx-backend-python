dd5e9435185fc39f0564e733327d44d4153ddd80
salmalah <s.salmalahmidi@gmail.com>
2024-01-11 12:02:11 -0800

    2. Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import math
+
+"""
+This is a module the create a function floor type float
+"""
+
+
+def floor(n: float) -> int:
+    """Add int"""
+    return math.floor(n)

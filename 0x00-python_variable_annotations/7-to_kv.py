b64b61baa71931de1d839781b8dac25b8d74d5b6
salmalah <122815714+salmalah@users.noreply.github.com>
2024-01-11 21:54:11 +0100

    Create 7-to_kv.py

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+This is a module function to_kv that takes a string k and an int OR float v as arguments
+"""
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple:
+    """Add int or float return Tuple"""
+    return (str, (v**2))

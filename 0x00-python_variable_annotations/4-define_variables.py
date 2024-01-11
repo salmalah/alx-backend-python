9d96ee174567b2e0b1ead469b98e29afcff6ea18
salmalah <s.salmalahmidi@gmail.com>
2024-01-11 12:22:41 -0800

    4. Define variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This is a module Define and annotate the following variables with
+the specified values
+"""
+
+
+school: str = "Holberton"
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True

609b80d1ad1aa61c88f629ca5d47566bfa7f7ab7
salmalah <122815714+salmalah@users.noreply.github.com>
2024-01-15 21:24:12 +0100

    fix pycodestyle 4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -8,6 +8,7 @@ from typing import List, Any
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
+
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     Create asyncio.Tasks for wait_random n times with the specified max_delay

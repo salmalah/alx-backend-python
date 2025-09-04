354f64d2f09cd5f398e2e14e39a329d2a8cb15dd
Salma Lahmidi <122815714+salmalah@users.noreply.github.com>
2025-09-04 15:08:09 +0100

    Update 1-batch_processing.py to yield users and include explicit return

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -29,8 +29,10 @@ def stream_users_in_batches(batch_size):
 
 
 def batch_processing(batch_size):
-    """Processes each batch and prints users older than 25"""
+    """Generator that yields users older than 25"""
     for batch in stream_users_in_batches(batch_size):
         for user in batch:
             if user['age'] > 25:
-                print(user)
+                yield user  
+    return  
+

af4d3b7508f0775bb858a32f5356cb52daa7578d
Salma Lahmidi <122815714+salmalah@users.noreply.github.com>
2025-09-19 21:35:54 +0100

    Update fixtures.py

--- 0x03-Unittests_and_integration_tests/fixtures.py
+++ 0x03-Unittests_and_integration_tests/fixtures.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python3
 
+
+"""Fixtures for GithubOrgClient integration tests."""
+
+org_payload = {
+    "login": "test_org",
+    "id": 1,
+    "repos_url": "https://api.github.com/orgs/test_org/repos"
+}
+
+repos_payload = [
+    {"name": "repo1", "license": {"key": "apache-2.0"}},
+    {"name": "repo2", "license": {"key": "mit"}},
+]
+
+expected_repos = ["repo1", "repo2"]
+
+apache2_repos = ["repo1"]
+
+
 TEST_PAYLOAD = [
   (
     {"repos_url": "https://api.github.com/orgs/google/repos"},

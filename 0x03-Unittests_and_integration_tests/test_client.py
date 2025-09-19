0b086ff479681054b4bff0b4b1e593a27363d43a
Salma Lahmidi <122815714+salmalah@users.noreply.github.com>
2025-09-19 22:10:29 +0100

    Update test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -7,7 +7,8 @@ import unittest
 from unittest.mock import patch, PropertyMock
 from parameterized import parameterized, parameterized_class
 from client import GithubOrgClient
-from fixtures import org_payload, repos_payload, expected_repos, apache2_repos
+from fixtures import TEST_PAYLOAD
+
 
 
 class TestGithubOrgClient(unittest.TestCase):

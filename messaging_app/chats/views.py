15ecfcd333f48685a24bcbd61e06bb996ecc4ca6
Salma Lahmidi <122815714+salmalah@users.noreply.github.com>
2025-09-26 17:48:54 +0100

    Update views.py

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,10 +1,10 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
-from rest_framework import status
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import CustomUser, Conversation, Message
 from .serializers import CustomUserSerializer, ConversationSerializer, MessageSerializer
+from .permissions import IsOwner, IsParticipant
 
 
 class CustomUserViewSet(viewsets.ModelViewSet):
@@ -14,31 +14,34 @@ class CustomUserViewSet(viewsets.ModelViewSet):
     
 
 class ConversationViewSet(viewsets.ModelViewSet):
-    queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsParticipant]
+
+    def get_queryset(self):
+        return Conversation.objects.filter(participants=self.request.user)
 
     def perform_create(self, serializer):
         serializer.save()
-        
-@action(detail=True, methods=['post'], permission_classes=[permissions.IsAuthenticated])
-def send_message(self, request, pk=None):
-    conversation = self.get_object()
-    data = request.data.copy()
-    data['sender_id'] = request.user.id
-    data['conversation'] = conversation.id 
 
-    serializer = MessageSerializer(data=data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    @action(detail=True, methods=['post'], permission_classes=[IsParticipant])
+    def send_message(self, request, pk=None):
+        conversation = self.get_object()
+        data = request.data.copy()
+        data['sender'] = request.user.id
+        data['conversation'] = conversation.id
 
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer = MessageSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class MessageViewSet(viewsets.ModelViewSet):
-    queryset = Message.objects.all()
     serializer_class = MessageSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsOwner]
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['conversation', 'sender']
+
+    def get_queryset(self):
+        return Message.objects.filter(sender=self.request.user)

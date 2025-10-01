1df88816f6ff2521f0f48d7c16c947dff008e05d
Salma Lahmidi <122815714+salmalah@users.noreply.github.com>
2025-10-01 16:25:34 +0100

    Update signals.py

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,6 +1,8 @@
 from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from .models import Message, Notification, MessageHistory
+from django.db.models.signals import post_delete
+from django.contrib.auth.models import User
 
 
 @receiver(post_save, sender=Message)
@@ -28,3 +30,16 @@ def log_message_edit(sender, instance, **kwargs):
             old_content=old_message.content,
             edited_by=instance.sender  # assuming sender edited the message
         )
+
+@receiver(post_delete, sender=User)
+def delete_related_data(sender, instance, **kwargs):
+    """
+    Automatically deletes all messages, notifications, and message histories
+    related to a user when the user account is deleted.
+    """
+    Message.objects.filter(sender=instance).delete()
+    Message.objects.filter(receiver=instance).delete()
+    
+    Notification.objects.filter(user=instance).delete()
+    
+    MessageHistory.objects.filter(edited_by=instance).delete()

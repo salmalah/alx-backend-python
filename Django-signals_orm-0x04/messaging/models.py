5e9ae046bdc0715f5eaf93b25bb501598dde8062
Salma Lahmidi <122815714+salmalah@users.noreply.github.com>
2025-10-01 16:18:47 +0100

    Update models.py

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -6,9 +6,20 @@ class Message(models.Model):
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+    edited = models.BooleanField(default=False)  # Track if the message was edited
 
     def __str__(self):
         return f"From {self.sender} to {self.receiver} at {self.timestamp}"
+
+
+class MessageHistory(models.Model):
+    message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='history')
+    old_content = models.TextField()
+    edited_at = models.DateTimeField(auto_now_add=True)
+    edited_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='edited_messages')
+
+    def __str__(self):
+        return f"Edit history of Message ID {self.message.id} at {self.edited_at}"
 
 
 class Notification(models.Model):
